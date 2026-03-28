8cad1648007831ac32092671dcac701bfe346a97
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-03-28 21:20:13 +0300

    feat(STT): add AddisAI STT integration

--- livekit/plugins/addisai/stt.py
+++ livekit/plugins/addisai/stt.py
@@ -2,8 +2,14 @@ import os
 from typing import Literal
 from dataclasses import dataclass
 
+import httpx
 import asyncio
+
+
 from livekit.agents import stt
+from livekit.agents.utils import AudioBuffer
+from livekit.agents.types import APIConnectOptions
+from livekit.agents.stt.types import SpeechEvent
 
 from livekit.agents.types import NOT_GIVEN, NotGivenOr
 from livekit.agents.utils import AudioBuffer, is_given
@@ -11,9 +17,11 @@ from livekit.agents.utils import AudioBuffer, is_given
 
 addisaiSttLanguages = Literal["am", "om"]
 
-@dataclass
+@dataclass(frozen=True)
 class STTOptions:
+    api_key: str
     language: addisaiSttLanguages
+    base_url: str = "https://api.addisassistant.com/api/v2/stt"
    
 
 class STT(stt.STT):
@@ -25,22 +33,35 @@ class STT(stt.STT):
         api_key:str | None = None
         ):
 
+        super().__init__(
+            capabilities=stt.STTCapabilities(
+                streaming=False,
+                interim_results=False, 
+                offline_recognize=True
+            )
+        )
+
         addisai_api_key = api_key if is_given(api_key) else os.environ.get("ADDISAI_API_KEY")
         if not addisai_api_key:
             raise ValueError(
-                "AddisAI API key is required, either as argument or set"
+                "AddisAI API key is required, either as argument or set "
                 "ADDISAI_API_KEY environment variable"
             )
 
-        if(is_given(language)):
-            self._language = language
-        else:
-            self._language = "am"
-       
+        self._opts = STTOptions(
+            api_key=addisai_api_key,
+            language=language,
+            base_url=base_url
+        )
 
+        self._client = httpx.AsyncClient(timeout=30.0)
+
+
+       
+    @property
     def model(self) -> str:
         return "Unkown"
-    
+    @property
     def provider(self) -> str:
         return "AddisAI"
     
@@ -48,5 +69,44 @@ class STT(stt.STT):
         if is_given(language):
             self._language = language
 
-    def stream(self):
-        pass
+
+    async def post(self, url,files,data):
+        headers = {
+            "Authorization": self._opts.api_key
+        }
+        response = await self._client.post(
+            url,
+            headers=headers,
+            files=files,
+            data=data
+        )
+        return response.json()
+
+            
+    async def _recognize_impl(self,audio:AudioBuffer,*,conn_options:APIConnectOptions) -> SpeechEvent:
+        wav_bytes = audio.to_wav_bytes()
+        language = self._opts.language
+        files = {
+            "file": ("audio.wav", wav_bytes, "audio/wav")
+        }
+        data = {
+            "language": str(language)
+        }
+
+        result = await self.post(self.base_url,files,data)
+        transcript = result.get("data").get("transcription")
+        return SpeechEvent(
+                type=SpeechEventType.FINAL_TRANSCRIPT,
+                alternatives=[
+                    SpeechData(
+                        text=transcript,
+                        language=language,
+                    )
+                ],
+            )
+        
+
+
+
+
+
